3052c97f992218e84314a78fbf973b6f8e47dd0d
Francisco Manríquez <francisco.manriquezn@usm.cl>
2024-05-24 14:43:01 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 
 
 def main():
+    builtins = ["exit", "echo", "type"]
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -11,6 +12,11 @@ def main():
             sys.exit(int(args[1]))
         elif args[0] == "echo":
             print(" ".join(args[1:]))
+        elif args[0] == "type":
+            if args[1] in builtins:
+                print(f"{args[1]} is a shell builtin")
+            else:
+                print(f"{args[1]}: not found")
         else:
             print(f"{args[0]}: command not found")
 
