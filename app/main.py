54d321d1e81cbd8f2505e63025bc5438c82fdf75
Francisco Manríquez <francisco.manriquezn@usm.cl>
2024-05-24 14:59:42 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,24 +1,52 @@
 import sys
+import os
+
+
+def on_type(args):
+    command = args[1]
+    if command in builtins:
+        print(f"{command} is a shell builtin")
+        return
+    
+    path = os.environ.get("PATH")
+    if path is None:
+        unknown(command)
+        return
+
+    for p in path.split(":"):
+        if not p.endswith("/"):
+            p += "/"
+        filename = p + command
+        print(filename)
+        if os.path.exists(filename):
+            print(f"{command} is {filename}")
+            return
+
+    unknown(command)
+
+builtins = {
+    "exit": lambda args: sys.exit(int(args[1])),
+    "echo": lambda args: print(" ".join(args[1:])),
+    "type": on_type,
+}
+unknown = lambda command: print(f"{command}: command not found")
+
 
 
 def main():
-    builtins = ["exit", "echo", "type"]
+
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
         args = input().split()
-        if args[0] == "exit":
-            sys.exit(int(args[1]))
-        elif args[0] == "echo":
-            print(" ".join(args[1:]))
-        elif args[0] == "type":
-            if args[1] in builtins:
-                print(f"{args[1]} is a shell builtin")
-            else:
-                print(f"{args[1]}: not found")
-        else:
-            print(f"{args[0]}: command not found")
+        command = args[0]
+        action = builtins.get(command, None)
+        if action is not None:
+            action(args)
+            continue
+
+        unknown(command)
 
 
 if __name__ == "__main__":
